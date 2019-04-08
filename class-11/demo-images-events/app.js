6d9bd711fad6f00bb27281ffd559ab23c67448dd
Sam Hamm <sam@codefellows.com>
2019-04-08 11:25:51 -0700

    Demos and lab

--- class-11/demo-images-events/app.js
+++ class-11/demo-images-events/app.js
@@ -21,3 +21,27 @@ new GoatPic('kissing-goat');
 new GoatPic('smiling-goat');
 new GoatPic('sweater-goat');
 new GoatPic('the-goat');
+
+// var previous;
+function showRandomGoat(){
+  var ramdom = Math.floor(Math.random() * allGoats.length);
+  console.log('currently showing before generating new ramdom', goatpic.alt);
+  while (goatpic.alt === allGoats[ramdom].name) {
+    var ramdom = Math.floor(Math.random() * allGoats.length);
+    console.log('duplicate found');
+  }
+  // previous = ramdom;
+  allGoats[ramdom].views += 1;
+  goatpic.src = allGoats[ramdom].filepath;
+  goatpic.alt = allGoats[ramdom].name;
+  goatpic.title = allGoats[ramdom].name;
+}
+
+function handleGoatClick(event) {
+  console.log(event.target);
+  showRandomGoat();
+}
+
+showRandomGoat();
+
+goatpic.addEventListener('click', handleGoatClick);
