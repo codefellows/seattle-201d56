d17bb59bd55801acff758eed30b0b7d6c0eff264
Sam Hamm <sam@codefellows.com>
2019-04-11 10:40:26 -0700

    Middle of class 4/11

--- class-14/demo-reinstantiation/app.js
+++ class-14/demo-reinstantiation/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var allCats = [];
+
+function Cat(name, age){
+  this.name = name;
+  this.age = age;
+  this.meow = function() {
+    console.log('MEEEOOOWWWWW!!!')
+  };
+  allCats.push(this);
+}
+
+new Cat ('Buddy', 16);
+new Cat ('Aloysius', 1);
